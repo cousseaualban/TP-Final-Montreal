d9e301799937734bfb6274de6f500db33033fd37
acousseau <acousseau@projetlineaire.com>
2023-02-07 12:09:05 +0100

    TP Final Montreal v15

--- compteur.js
+++ compteur.js
@@ -1,2 +1,7 @@
-function getRandomNumber() {
+const number = document.getElementById('randomNumber');
+
+const random = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+number.innerHTML = random(1, 200);
